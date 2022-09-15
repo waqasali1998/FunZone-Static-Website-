a90e55a1b7483cf948776862fe126cd4fe55b233
waqas <waqas1998ali@hotmail.com>
2022-09-15 15:55:10 +0500

    first commit

---
+++ pics_files/vlb_engine/vlbdata1.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function(){ window.Lightbox = new jQuery().visualLightbox({autoPlay:false,borderSize:57,classNames:'vlightbox1',closeLocation:'top',descSliding:false,enableRightClick:false,enableSlideshow:true,prefix:'vlb1',resizeSpeed:7,slideTime:4,startZoom:true}) });
